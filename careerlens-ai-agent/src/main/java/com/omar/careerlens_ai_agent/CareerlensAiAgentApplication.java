ec3ad75d3bc1d6c670530b2b9fcdeb6612d310a7
DrissiOmar <omar.drissi@esprit.tn>
2026-07-20 19:52:51 +0100

    feat(init): set up CareerLens AI assistant with Spring Boot and Embabel

---
+++ careerlens-ai-agent/src/main/java/com/omar/careerlens_ai_agent/CareerlensAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.omar.careerlens_ai_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerlensAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerlensAiAgentApplication.class, args);
+	}
+
+}
