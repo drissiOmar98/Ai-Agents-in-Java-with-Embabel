96539ba0c7358064777e770f762437de532d0512
DrissiOmar <omar.drissi@esprit.tn>
2026-07-29 00:23:59 +0100

    feat(wattwise): initialize WattWise AI Home Energy Tracker project

---
+++ home-energy-tracker/src/main/java/com/omar/home_energy_tracker/HomeEnergyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.omar.home_energy_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeEnergyTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeEnergyTrackerApplication.class, args);
+	}
+
+}
